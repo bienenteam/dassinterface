3de2b502aecc93acebd2cc54701f15f78d50f477
meitoasty <fabian.meise@unitybox.de>
2016-10-01 17:19:18 +0200

    push

--- _attachments/js/filter.js
+++ _attachments/js/filter.js
@@ -1,18 +1,40 @@
 function FilterItems(e) {
-console.log(e.id);
+  var postsection = document.getElementById("postsection");
+  postsection.innerHTML="";
 
 
+  if(e.parentNode.classList.contains("active-source")){
+    e.parentNode.classList.remove("active-source");
+    db.setHideFeed(e.id);
+  }
+  else{
+    e.parentNode.classList.add("active-source");
+    db.setShowFeed(e.id);
+  }
 }
 
 
-function CreateFeed(){
+function CreateFeedItem(item){
 
-  var template = _.template("<hr><header class='post-header'><h2 class='post-title'><%= titel %></h2>  <p class='post-meta'><%= meta %></p></header><div class='post-description'><p><%= description %></p></div></section>");
-  //console.log(template);
-  var render = template({titel: 'Ich bin ein Titel'});
+  var template = _.template("<hr><header class='post-header'><h2 class='post-title'><%= title %></h2>  <p class='post-meta'><%= link %></p></header><div class='post-description'><p><%= summary %></p></div></section>");
+  console.log(item);
+  var render = template(item);
   var para = document.createElement("section");
   para.className="post";
   para.innerHTML= render;
-  document.getElementById("postsection").appendChild(para);
+  var section = document.getElementById("postsection");
+  section.insertBefore( para, section.firstChild );
+}
 
+
+function CreateFeed(feed){
+
+  var template = _.template("<a class='pure-button' onclick='FilterItems(this)' id='<%= id %>'><%= title %></a>");
+  console.log(feed);
+  var render = template(feed);
+  var para = document.createElement("li");
+  para.className="nav-item active-source";
+  para.innerHTML= render;
+  var section = document.getElementById("nav-list");
+  section.insertBefore( para, section.firstChild );
 }
