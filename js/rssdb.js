14beeda78136eeec85e280f2aff1aa54b89c426c
Max Polster <max.polster@id-p.de>
2016-10-01 15:09:36 +0200

    Fixed issues.

--- js/rssdb.js
+++ js/rssdb.js
@@ -1,43 +1,59 @@
 
 function RssDB(intf) {
-	var itemSource = "http://93.180.156.188:5984/simdata";
-	var itemFields = ["_id", "_rev", "title"];
-	var pollInterval = 1500;
-	var updateHandle = null;
-	var lastSequence = 0;
+	var store = {
+		database: "http://93.180.156.188:5984/simdata",
+		pollInterval: 1500,
+		updateHandle: null,
+		lastSequence: 0,
+		didFirstPoll: false
+	};
 
 
 
-
+	function pollFirstChangesFeed() {
+		var query = "include_docs=true&since=" + store.lastSequence
+			+ "&limit=1&descending=true";
+		request("GET", store.database + "/_changes?" + query, function(response) {
+			if (!store.didFirstPoll) {
+				store.didFirstPoll = true;
+				store.lastSequence = Math.max(0, response.data.last_seq - 50);
+			}
+		});
+	}
 
 	function pollChangedFeedItems() {
-		var query = "filter=_selector&include_docs=true&since=" + lastSequence;
-		request("POST", itemSource + "/_changes?" + query, function(response) {
-			var results = response.data.results;
-			for(var i = 0; i < results.length; i++) {
-				intf.addFeedItem(results[i].doc);
-			}
-		}, {
-			selector: { type: "item" }
-		});
+		if (store.didFirstPoll) {
+			var query = "include_docs=true&since=" + store.lastSequence
+				+ "&limit=50";
+			request("GET", store.database + "/_changes?" + query, function(response) {
+				var results = response.data.results;
+				for(var i = 0; i < results.length; i++)
+					if (results[i].doc.type == "item")
+						intf.addFeedItem(results[i].doc);
+
+				store.lastSequence = response.data.last_seq;
+			});
+		} else {
+			pollFirstChangesFeed();
+		}
 	}
 
 
 
 	function update() {
-		updateHandle = setTimeout(update, pollInterval);
+		store.updateHandle = setTimeout(update, store.pollInterval);
 		pollChangedFeedItems();
 	}
 
 	function start() {
-		if (updateHandle == null)
+		if (store.updateHandle == null)
 			update();
 	}
 
 	function stop() {
-		if (updateHandle != null) {
-			clearTimeout(updateHandle);
-			updateHandle = null;
+		if (store.updateHandle != null) {
+			clearTimeout(store.updateHandle);
+			store.updateHandle = null;
 		}
 	}
 
@@ -51,15 +67,16 @@ function RssDB(intf) {
 				data: JSON.parse(rx.responseText)
 			});
 		});
-		rx.send(body);
+
+		if (body)
+			rx.send(JSON.stringify(body));
+		else
+			rx.send();
 	}
 
 	this.start = start;
 	this.stop = stop;
 }
-
-
-
 
 
 
