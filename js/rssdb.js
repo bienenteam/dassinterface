c8f50f37e8070f7f5907172cd1b9c01aa3550cf9
Max Polster <max.polster@id-p.de>
2016-10-01 12:42:34 +0200

    Initial polling & automated updates.

--- js/rssdb.js
+++ js/rssdb.js
@@ -1,4 +1,81 @@
 
+function RssDB(intf) {
+	var itemSource = "testdb";
+	var itemFields = ["_id", "_rev", "title"];
+	var pollInterval = 1500;
+	var updateHandle = null;
+
+	function pollInitialFeedItems() {
+		request("POST", itemSource + "/_find", function(response) {
+			var feedItems = response.data.docs;
+			for(var i = 0; i < feedItems.length; i++) {
+				intf.addFeedItem(feedItems[i]);
+			}
+		}, {
+			selector: { type: "item" },
+			fields: itemFields,
+			sort: [{published: "desc"}],
+			limit: 50,
+			skip: 0
+		});
+	}
+
+	function pollChangedFeedItems() {
+		request("POST", itemSource + "/_changes?filter=_selector&include_docs=true", function(response) {
+			var results = response.data.results;
+			for(var i = 0; i < results.length; i++) {
+				intf.addFeedItem(results[i].doc);
+			}
+		}, {
+			selector: { type: "item" }
+		});
+	}
+
+
+
+	function update() {
+		updateHandle = setTimeout(update, pollInterval);
+		pollChangedFeedItems();
+	}
+
+	function start() {
+		if (updateHandle == null)
+			update();
+	}
+
+	function stop() {
+		if (updateHandle != null) {
+			clearTimeout(updateHandle);
+			updateHandle = null;
+		}
+	}
+
+
+
+	function request(method, url, callback, body) {
+		var rx = new XMLHttpRequest();
+		rx.open(method, url);
+		rx.addEventListener('load', function() {
+			callback({
+				data: JSON.parse(rx.responseText)
+			});
+		});
+		rx.send(body);
+	}
+
+	pollInitialFeedItems();
+
+	this.start = start;
+	this.stop = stop;
+}
+
+
+
+
+
+
+
+
 function RssDBTest(intf) {
 	var i = 0;
 
